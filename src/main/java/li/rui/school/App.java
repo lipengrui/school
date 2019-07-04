0a43b9abd648b561eb21983076e173244d4484fe
lipengrui <18347451522@163.com>
2019-07-04 22:47:22 +0800

    school 家

--- src/main/java/li/rui/school/App.java
+++ src/main/java/li/rui/school/App.java
@@ -1,5 +1,10 @@
 package li.rui.school;
 
+import java.io.Console;
+
+import util.AudioUtils;
+import util.MP3ConvertPCM;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        try {
+        	AudioUtils utils  = AudioUtils.getInstance();
+            utils.convertMP32Pcm("D:\\WORK\\cocos\\school\\resource\\test.mp3", "D:\\WORK\\cocos\\school\\resource\\test.pcm");
+//			MP3ConvertPCM.mp3Convertpcm("D:\\WORK\\cocos\\school\\resource\\test.mp3", "D:\\WORK\\cocos\\school\\resource\\test.pcm");
+		} catch (Exception e) {
+			System.out.println("error");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
